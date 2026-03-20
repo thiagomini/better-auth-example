509dffefc577534eb24d7cc72311754e9e3e02d4
Thiago Martins <thiago_valentim1@hotmail.com>
2026-03-20 17:44:13 -0300

    chore: remove .env

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,12 @@
 import { betterAuth } from 'better-auth';
+import { Pool } from 'pg';
 
 export const auth = betterAuth({
   trustedOrigins: ['http://localhost:3000'],
   emailAndPassword: {
     enabled: true,
   },
+  database: new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
 });
