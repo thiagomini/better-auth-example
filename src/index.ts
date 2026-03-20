1d0fbe4162e12557be38f4bf82a95ad94f17190f
Thiago Martins <thiago_valentim1@hotmail.com>
2026-03-20 17:35:18 -0300

    feat: add bruno auth example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { fromNodeHeaders, toNodeHandler } from 'better-auth/node';
+import { auth } from '#lib/auth.ts';
 
 const app = express();
 const port = 3000;
@@ -6,6 +8,17 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
+
+app.get('/api/me', async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  return res.json(session);
+});
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
